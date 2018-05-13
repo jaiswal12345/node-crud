7b0fdf777e5c843d86b0cf448db93a2be175a540
piyush <jaiswal.piyush72@gmail.com>
2018-05-13 23:28:58 +0530

    added dynamic port

--- index.js
+++ index.js
@@ -10,8 +10,9 @@ var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
 
 var app = express();
+var port = process.env.PORT|| 3000;
 app.use(bodyParser.json());
-app.listen(3000,function(){
+app.listen(port,function(){
     console.log("Server started");
 });
 mongoose.connection.openUri("mongodb://admin:admin@ds119070.mlab.com:19070/product-demo");
